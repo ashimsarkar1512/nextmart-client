f579ed547d01795fd95b5e931fa1cc8a75ea7a42
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-04-27 22:44:57 +0600

    create the register form and zod validation in fornend

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,11 @@
+
+
+const HomePage = () => {
+            return (
+                        <div>
+                           <h1>wellcome to the nextmart</h1>         
+                        </div>
+            );
+};
+
+export default HomePage;
---
+++ src/services/AuthService/index.ts
@@ -0,0 +1,11 @@
+export const registerUser=async (userData)=>{
+            const res=await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/user`,{
+             method:"POST",
+             headers:{
+                        "Content-Type":"aplication/json"
+             },
+             body:JSON.stringify(userData)
+            })
+
+            return res.json()
+}
